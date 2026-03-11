01e084be021b9638645d56b83dda3366dbd91d55
Lotvil <b.mcsinen@gmail.com>
2026-03-11 11:05:03 +0100

    Started on breaking blocks

---
+++ GYAR_Spel/scripts/BlockData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name BlockData
+
+@export var tile_name: String = "default_block"
+@export var health: int = 3
+@export var atlas_coords: Array[Vector2i] = []
+@export var source_id: int = 0
