193b404c986e0993958e83b3ee6e51683dae869a
Philip <philipsode07@gmail.com>
2026-03-17 12:01:19 +0100

    kamera y door fix

--- GYAR_Spel/scripts/door.gd
+++ GYAR_Spel/scripts/door.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var enterSide = null
+var enterSide :String
+var x_lock :bool
+
 
 @onready var player: CharacterBody2D = $"../../Player"
 @onready var camera_2d: Camera2D = $"../../Player/Camera2D"
@@ -13,6 +15,7 @@ func _on_stop_area_body_entered(body: Node2D) -> void:
 		print("touch")
 		camera_2d.top_level = true
 		camera_2d.global_position = pos
+		x_lock = true
 
 
 func _on_stop_area_body_exited(body: Node2D) -> void:
@@ -21,6 +24,7 @@ func _on_stop_area_body_exited(body: Node2D) -> void:
 		camera_2d.top_level = false
 		camera_2d.global_position.x = player.global_position.x
 		camera_2d.global_position.y = player.global_position.y -324
+		x_lock = false
 
 
 func _on_door_area_body_entered(body: Node2D) -> void:
@@ -41,3 +45,8 @@ func _on_door_area_body_exited(body: Node2D) -> void:
 			
 		if enterSide == "right":
 			camera_2d.global_position.x = $markerLeft.global_position.x
+
+
+func _process(delta: float) -> void:
+	if x_lock == true:
+		camera_2d.global_position.y = player.global_position.y -324
