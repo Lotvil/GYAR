052a0355c33125aaebe1ab27e647304cbc327c4f
Philip <philipsode07@gmail.com>
2026-03-18 15:00:10 +0100

    Door funkar perfekt nu
    hatar er två

--- GYAR_Spel/scripts/door.gd
+++ GYAR_Spel/scripts/door.gd
@@ -3,6 +3,8 @@ extends Node2D
 var enterSide :String
 var x_lock :bool
 
+var touchLeft: bool
+var touchRight: bool
 
 @onready var player: CharacterBody2D = $"../../Player"
 @onready var camera_2d: Camera2D = $"../../Player/Camera2D"
@@ -11,42 +13,42 @@ var x_lock :bool
 
 func _on_stop_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var pos = camera_2d.global_position
-		print("touch")
-		camera_2d.top_level = true
-		camera_2d.global_position = pos
-		x_lock = true
+		print("touchLeft")
+		touchLeft = true
 
 
 func _on_stop_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		print("exit")
-		camera_2d.top_level = false
-		camera_2d.global_position.x = player.global_position.x
-		camera_2d.global_position.y = player.global_position.y -324
-		x_lock = false
+		print("exitLeft")
+		touchLeft = false
+		
 
 
 func _on_door_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if player.global_position > global_position:
-			print("entered from right")
-			enterSide = "right"
-		else:
-			print("entered from left")
-			enterSide = "left"
+		print("touchRight")
+		touchRight = true
 
 
 func _on_door_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if enterSide == "left":
-			camera_2d.global_position.x = $markerRight.global_position.x
-			
-			
-		if enterSide == "right":
-			camera_2d.global_position.x = $markerLeft.global_position.x
+		print("exitRight")
+		touchRight = false
 
 
 func _process(_delta: float) -> void:
-	if x_lock == true:
+	if touchRight or touchLeft:
+		camera_2d.top_level = true
 		camera_2d.global_position.y = player.global_position.y -324
+		
+	
+	else:
+		camera_2d.top_level = false
+		camera_2d.global_position.x = player.global_position.x
+		camera_2d.global_position.y = player.global_position.y -324
+		
+	
+	if touchLeft and !touchRight:
+		camera_2d.global_position.x = $markerLeft.global_position.x
+	if touchRight and !touchLeft:
+		camera_2d.global_position.x = $markerRight.global_position.x
